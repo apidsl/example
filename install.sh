45afc8214b04fbd85551da6157f349dfaaea74e4
tom-sapletta-com <tom@sapletta.com>
2022-06-07 22:55:40 +0200

    update docs

---
+++ install.sh
@@ -0,0 +1,6 @@
+curl https://raw.githubusercontent.com/apifork/bash/main/apifork.sh -o apifork
+./apifork init
+./apifork install
+curl https://raw.githubusercontent.com/flatedit/bash/main/flatedit.sh -o flatedit
+./flatedit init
+
